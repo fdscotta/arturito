d54d55dacead2956d4154ad557a87763bc4c2fe4
fdscotta <francisco.scotta@gmail.com>
2017-07-06 22:00:56 -0300

    Primera subida
    pancho

---
+++ portal/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-07-04 14:24:23 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-07-04T14:24:23+00:00	INFO 190.183.238.30	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
---
+++ portal/administrator/logs/joomla_update.php
@@ -0,0 +1,15 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-07-04 14:19:46 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-07-04T14:19:46+00:00	INFO 190.183.238.30	update	Update started by user Super User (826). Old version is 3.7.2.
+2017-07-04T14:19:47+00:00	INFO 190.183.238.30	update	Downloading update file from http://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.7.3-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20170704%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20170704T141952Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=618acaa1289d0efd99a3df91ccaf6e3cc20672e2f78ac2fe8b4d278fb76f5d95.
+2017-07-04T14:19:52+00:00	INFO 190.183.238.30	update	File Joomla_3.7.3-Stable-Update_Package.zip successfully downloaded.
+2017-07-04T14:19:53+00:00	INFO 190.183.238.30	update	Starting installation of new version.
+2017-07-04T14:19:57+00:00	INFO 190.183.238.30	update	Finalising installation.
+2017-07-04T14:19:57+00:00	INFO 190.183.238.30	update	Ran query from file 3.7.3-2017-06-03. Query text: ALTER TABLE `#__menu` MODIFY `checked_out_time` datetime NOT NULL DEFAULT '0000-.
+2017-07-04T14:19:57+00:00	INFO 190.183.238.30	update	Deleting removed files and folders.
+2017-07-04T14:20:06+00:00	INFO 190.183.238.30	update	Cleaning up after installation.
+2017-07-04T14:20:06+00:00	INFO 190.183.238.30	update	Update to version 3.7.3 is complete.
